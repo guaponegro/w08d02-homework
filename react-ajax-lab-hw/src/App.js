9873ee64a4335c5ed37f92cc2c24ea8ef1ea2237
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-30 19:58:55 -0600

    tried to get the API to work but couldn't get anything on the page

--- react-ajax-lab-hw/src/App.js
+++ react-ajax-lab-hw/src/App.js
@@ -10,8 +10,26 @@ class App extends Component {
 
     this.state = {
       logged: false,
-      username: ''
+      username: '',
+      weather: [],
     }
+  }
+  getWeather = async () => {
+    try{
+      const weather = await fetch('http://api.apixu.com/v1/current.json?key=269e174cd49646928a912813183110&q=Denver');
+      const weatherJson = await weather.json();
+      return weatherJson;
+    } catch(err){
+      console.log(err);
+    }
+  }
+  componentDidMount(){
+    this.getWeather().then((weather) => {
+      console.log(weather, 'this is the weather');
+      this.setState({weather: weather});
+    }).catch((err) => {
+      console.log(err);
+    })
   }
   handleLogin = (username, loggedIn) => {
     console.log(username, loggedIn, ' in handleLogin');
@@ -24,7 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.logged ? <WeatherContainer username={this.state.username}/> : <Login handleLogin={this.handleLogin} />}
+        {this.state.logged ? <WeatherContainer temperature={this.state.temperature} wind={this.state.wind} humidity={this.state.humidity} /> : <Login handleLogin={this.handleLogin} />}
       </div>
     );
   }
--- react-ajax-lab-hw/src/WeatherContainer/WeatherContainer.js
+++ react-ajax-lab-hw/src/WeatherContainer/WeatherContainer.js
@@ -3,10 +3,22 @@ import React, { Component } from 'react';
 
 class WeatherContainer extends Component {
     render(){
+        const weatherList = this.props.weather.map((weather, i) => {
+            return (
+                <li key={i}>
+                Humidity: {weather.humidity}<br/>
+                Temperature: {weather.temperature}<br/>
+                Wind: {weather.wind}<br/>
+                </li>
+            )
+        });
         return (
             <div>
                 <h1>Hi {this.props.username}</h1>
                 <h2>Weather Page</h2>
+                <ul>
+                    {WeatherContainer}
+                </ul>
             </div>
         )
     }
